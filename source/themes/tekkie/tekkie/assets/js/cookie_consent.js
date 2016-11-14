6c1570cdf9631551701908021e468438d76469de
Georgiana Gligor <gb@tekkie.ro>
2016-11-14 19:41:51 +0200

    restore analytics and cookie consent

---
+++ source/themes/tekkie/tekkie/assets/js/cookie_consent.js
@@ -0,0 +1,7 @@
+window.cookieconsent_options = {
+  "message":"This website uses cookies to ensure you get the best experience on our website",
+  "dismiss":"Got it!",
+  "learnMore":"More info",
+  "link":"www.tekkie.ro/cookies",
+  "theme":"dark-floating"
+};
